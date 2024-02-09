7736e89796541762a711ffe6564fd23fd272f95d
sj1928 <bhoisiba282@gmail.com>
2024-02-09 19:52:24 +0530

    Basic printing Question

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("You can print anything using this....");
+        System.out.println("System.out.println();");
+    }
+}
+
